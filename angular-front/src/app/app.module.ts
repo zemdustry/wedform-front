bb286ddf81970b398f2a19cb185cd09b97e3f102
CodingClem <c.gibertpro@gmail.com>
2023-01-21 19:26:52 +0100

    Shaping header with bootstrap and navigation

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -3,25 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatIconModule} from '@angular/material/icon';
-import { FormComponent } from './form/form.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FormComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
     MatIconModule
   ],
   providers: [],
---
+++ angular-front/src/app/shared/index.ts
@@ -0,0 +1,4 @@
+import { HeaderComponent } from "./header/header.component";
+
+export const declarationComponents: any[] = [HeaderComponent];
+export const exportComponents : any[] = [HeaderComponent];
