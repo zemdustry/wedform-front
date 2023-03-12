54ab33ea12b24a797911dcfdc7f9d7e18b5e02a0
CodingClem <c.gibertpro@gmail.com>
2023-03-12 01:14:57 +0100

    feature#12: FormGroup and controls dev

---
+++ angular-front/src/app/register/components/register/options.ts
@@ -0,0 +1,157 @@
+export interface Options {
+  key: string;
+  value: string;
+}
+
+export const attendOptions: Options[] = [
+  {
+    key: "yes",
+    value: "Yes"
+  },
+  {
+    key: "no",
+    value: "No"
+  },
+  {
+    key: "maybe",
+    value: "Maybe..."
+  }
+]
+
+export const arrivalOptions: Options[] = [
+  {
+    key: "already",
+    value: "I am already in the area"
+  },
+  {
+    key: "17",
+    value: "The D-Day, Saturday 17th of June"
+  },
+  {
+    key: "16",
+    value: "The Friday 16th of June"
+  },
+  {
+    key: "before",
+    value: "Many days before"
+  }
+]
+
+export const transportOptions: Options[] = [
+  {
+    key: "car",
+    value: "By car"
+  },
+  {
+    key: "taxi",
+    value: "By taxi"
+  },
+  {
+    key: "train",
+    value: "By train"
+  },
+  {
+    key: "idk",
+    value: "I don't know"
+  },
+]
+
+export const transportShareOptions: Options[] = [
+  {
+    key: "yes",
+    value: "Yes"
+  },
+  {
+    key: "no",
+    value: "No"
+  }
+]
+
+export const eventOptions: Options[] = [
+  {
+    key: "morning-ceremony",
+    value: "Only morning Ceremony in the City Hall of Le Grau du Roi which starts at 11am"
+  },
+  {
+    key: "afternoon-ceremony",
+    value: "Only afternoon Ceremony in the Mas de la Montille – Aigues Morte which starts at 2pm"
+  },
+  {
+    key: "boat",
+    value: "Only reception on the Canal Boat which starts at 6pm"
+  },
+  {
+    key: "ceremonies",
+    value: "Both Ceremonies only"
+  },
+  {
+    key: "morning-ceremony-and-boat",
+    value: "Morning Ceremony and boat"
+  },
+  {
+    key: "afternoon-ceremony-and-boat",
+    value: "Afternoon Ceremony and Reception boat"
+  },
+  {
+    key: "all",
+    value: "I'm going to participate in everything! 🎉"
+  },
+]
+
+export const dietaryOptions: Options[] = [
+  {
+    key: "yes",
+    value: "Yes"
+  },
+  {
+    key: "no",
+    value: "No"
+  }
+]
+
+
+export const musicOptions: Options[] = [
+  {
+    key: "general",
+    value: "General"
+  },
+  {
+    key: "dance-electro",
+    value: "Dance/Electro"
+  },
+  {
+    key: "pop-rock",
+    value: "Pop/Rock"
+  },
+  {
+    key: "funk",
+    value: "Funk"
+  },
+  {
+    key: "rap",
+    value: "Rap"
+  },
+  {
+    key: "disco",
+    value: "Disco"
+  },
+  {
+    key: "latino",
+    value: "Latino"
+  },
+  {
+    key: "80",
+    value: "80s"
+  }
+]
+
+export const brunchOptions: Options[] = [
+  {
+    key: "yes",
+    value: "Yes"
+  },
+  {
+    key: "no",
+    value: "No"
+  }
+]
--- angular-front/src/app/register/components/register/register.component.ts
+++ angular-front/src/app/register/components/register/register.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { FormField } from '../../services/form-field';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { FormFieldControllerService } from '../../services/form-field-controller.service';
+import { arrivalOptions, attendOptions, brunchOptions, dietaryOptions, eventOptions, transportOptions, transportShareOptions } from './options';
 
 @Component({
   selector: 'app-register',
@@ -11,9 +10,91 @@ import { FormFieldControllerService } from '../../services/form-field-controller
   providers: [FormFieldControllerService]
 })
 export class RegisterComponent {
-  title = 'AngularDynamicForms';
-  formFields: Observable<FormField<any>[]>;
-  constructor(formFieldControllerService: FormFieldControllerService) {
-    this.formFields = formFieldControllerService.getFormFields();
+  guestForm = new FormGroup({
+    name: new FormControl(),
+    surname: new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl(),
+    attend: new FormControl(),
+    persons: new FormControl(),
+    personFormGroups: new FormArray([]),
+    children: new FormControl(),
+    childFormGroups: new FormArray([]),
+    arrival: new FormControl(),
+    transportation: new FormControl(),
+    transportShare: new FormControl(),
+    event: new FormControl(),
+    dietary: new FormControl(),
+    dietaryDetail: new FormControl(),
+    song: new FormControl(),
+    brunch: new FormControl(),
+    comment: new FormControl()
+  });
+
+  public attendOptions = attendOptions;
+  public arrivalOptions = arrivalOptions;
+  public transportOptions = transportOptions;
+  public transportShareOptions = transportShareOptions;
+  public eventOptions = eventOptions;
+  public dietaryOptions = dietaryOptions;
+  public brunchOptions = brunchOptions;
+
+  public willAttend: boolean = true;
+  public numberOfPeople: number = 1;
+  peopleFormGroups: FormGroup[] = [];
+  public numberOfChildren: number = 0;
+  childFormGroups: FormGroup[] = [];
+  public transportation: string;
+  public dietary: boolean;
+  songs: string[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.guestForm.get('attend').valueChanges.subscribe(value => {
+      this.willAttend = value == "no" ? false : true;
+    })
+
+    this.guestForm.get('persons').valueChanges.subscribe(peopleCount => this.numberOfPeople = peopleCount)
+    this.guestForm.get('children').valueChanges.subscribe(childrenCount => {
+      if (this.numberOfChildren < childrenCount) {
+        const childForm = new FormGroup({
+          name: new FormControl(''),
+          surname: new FormControl('')
+        });
+        this.childFormGroups.push(childForm);
+      } else if (this.numberOfChildren > childrenCount) {
+        this.childFormGroups.pop();
+      }
+      this.numberOfChildren = childrenCount;
+    })
+
+    this.guestForm.get('transportation').valueChanges.subscribe(transport => this.transportation = transport);
+    this.guestForm.get('dietary').valueChanges.subscribe(dietary => this.dietary = dietary = 'yes' ? true : false)
+  }
+
+  numSequence(n: number): Array<number> {
+    return Array(n);
+  }
+
+  addSong() {
+    const songValue = this.guestForm.get('song').value;
+    if (songValue !== null && songValue !== '') {
+      this.songs.push(songValue);
+      this.guestForm.get('song').reset();
+    }
+  }
+
+  removeSong(song: string) {
+    const indexToRemove = this.songs.indexOf(song); // find the index of the element to remove
+    if (indexToRemove > -1) { // check if the element was found
+      this.songs.splice(indexToRemove, 1); // remove the element at the found index
+    }
+  }
+
+  onFormSubmit(): void {
+    this.guestForm.patchValue({
+      personFormGroups: new FormArray(this.peopleFormGroups),
+      childFormGroups: new FormArray(this.childFormGroups)
+    });
+    console.log('Name:' + this.guestForm.get('name').value);
   }
 }
