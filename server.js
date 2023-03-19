c896fec474295f642fe5394c5787d5c3b2610e53
CodingClem <c.gibertpro@gmail.com>
2023-03-19 23:07:34 +0100

    express dist

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/web-form'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname, 'dist/web-form', 'index.html'));
+res.sendFile(path.join(__dirname, 'dist/web-form'));
 });
 
 // Start the app by listening on the default Heroku port
