7babb8b3a5e374bb98ba1e8ecd0b91be998e27a9
CodingClem <c.gibertpro@gmail.com>
2023-03-21 20:22:12 +0100

    clean after dockerise project

--- src/app/home/components/countdown-custom/countdown-custom.component.ts
+++ src/app/home/components/countdown-custom/countdown-custom.component.ts
@@ -5,56 +5,7 @@ import { delay } from 'rxjs';
 @Component({
   selector: 'app-countdown-custom',
   templateUrl: './countdown-custom.component.html',
-  styleUrls: ['./countdown-custom.component.scss'],
-  //TODO set flipclock animation for top and bottom palette
-  animations: [
-    trigger('flipTop', [
-      transition('* => *', [
-        animate('500ms 500ms cubic-bezier(.37,.01,.94,.35)', keyframes([
-          style({
-            transformOrigin: 'center bottom',
-            transform: 'rotateX(0deg)',
-            opacity: 0.99,
-            zIndex: 2
-          }),
-          style({
-            transform: 'rotateX(-90deg)',
-            opacity: 0
-          })
-        ]))
-      ])
-    ]),
-    trigger('flipBottom', [
-      transition('* => *', [
-        animate('500ms 900ms cubic-bezier(.17,.67,.2,1.12)',
-          keyframes([
-            style({
-              transformOrigin: 'center top',
-              transform: 'rotateX(90deg)',
-              opacity: 0,
-              zIndex: -1,
-              offset: 0
-            }),
-            style({
-              transform: 'rotateX(0deg)',
-              opacity: 0.99,
-              zIndex: 5,
-              offset: 1
-            })
-          ])
-        )
-      ])
-    ]),
-    trigger("flip",[
-      transition('*=>*',[
-      animate(".7s",keyframes([
-        style({transform:"rotateX(0deg)",offset: 0}),
-        style({transform:"rotateX(-90deg)",offset: .5}),
-        style({transform:"rotateX(-180deg)",offset: 1}),
-      ]))
-    ])
-  ])
-  ]
+  styleUrls: ['./countdown-custom.component.scss']
 })
 
 export class CustomCountdownComponent {
@@ -74,15 +25,8 @@ export class CustomCountdownComponent {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    // if(this.isNotSeconds) {
-    //   setTimeout(() => {
-    //     this.lastValue = this.currentValue;
-    //     this.currentValue = this.value;
-    //   }, 1000);
-    // } else {
       this.lastValue = this.currentValue;
       this.currentValue = this.value;
-    // }
   }
 
   public transformTime(time: number) : string {
