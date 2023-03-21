3ab75fac8a756e3f8873ca2cc7aca99abb23c664
CodingClem <c.gibertpro@gmail.com>
2023-03-21 01:42:55 +0100

    Clean and node server

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterModule } from '@angular/router';
 import * as sharedComponents from '.';
-import { AgePipe } from './pipes/age-pipe';
-import { BendTitleTopComponent } from './bend-title-top/bend-title-top.component';
 import { BendTitleBottomComponent } from './bend-title-bottom/bend-title-bottom.component';
+import { BendTitleTopComponent } from './bend-title-top/bend-title-top.component';
 
 @NgModule({
   declarations: [...sharedComponents.declarationComponents, BendTitleTopComponent, BendTitleBottomComponent],
-  imports: [CommonModule, RouterModule, MatSlideToggleModule],
+  imports: [CommonModule, RouterModule],
   exports: [...sharedComponents.exportComponents]
 })
 export class SharedModule {
